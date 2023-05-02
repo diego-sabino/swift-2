2301d5de5e223c398ec729a8acbe920c35310e15
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-02 19:53:02 -0300

    atividade 1

---
+++ iQuiz/iQuiz/PerformanceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PerformanceViewController.swift
+//  iQuiz
+//
+//  Created by Diego Sabino on 02/05/23.
+//
+
+import UIKit
+
+class PerformanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
